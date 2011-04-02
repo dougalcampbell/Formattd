43258478252c4a71f2e5e2ab1ebc352cedff217a
Dougal Campbell <dougal@woody.xen.prgmr.com>
2011-04-02 07:48:29 -0400

    Single and comment templates.

---
+++ single.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div id="content">
+<div class="spacer">
+<?php
+	get_template_part('loop', 'single');	
+?>
+</div><!--.spacer-->
+</div><!--#content-->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
