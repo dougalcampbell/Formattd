d627aa23a8fa48f7d61976a80105a48419324a6f
Dougal Campbell <dougal@woody.xen.prgmr.com>
2011-04-22 12:05:36 -0400

    rename primary widget area

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,7 @@
 	 * then the sidebar simply doesn't exist, so we'll hard-code in
 	 * some default sidebar stuff just in case.
 	 */
-	if ( ! dynamic_sidebar( 'primary-asides' ) ) : ?>
+	if ( ! dynamic_sidebar( 'primary-aside' ) ) : ?>
 
 			<li id="search" class="widget-container widget_search">
 				<?php get_search_form(); ?>
