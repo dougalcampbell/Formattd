ffc534569c3baf0ed509399e050391a82a1ccb45
Dougal Campbell <dougal@woody.xen.prgmr.com>
2011-04-21 22:03:45 -0400

    page template

---
+++ loop-page.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * The loop that displays posts.
+ *
+ * The loop displays the posts and the post content.  See
+ * http://codex.wordpress.org/The_Loop to understand it and
+ * http://codex.wordpress.org/Template_Tags to understand
+ * the tags used in it.
+ *
+ * This can be overridden in child themes with loop.php or
+ * loop-template.php, where 'template' is the loop context
+ * requested by a template. For example, loop-index.php would
+ * be used if it exists and we ask for the loop with:
+ * <code>get_template_part( 'loop', 'index' );</code>
+ *
+ * @package WordPress
+ * @subpackage Initializr
+ * @since Initializr 1.0
+ */
+?>
+
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+	<nav id="nav-above" class="navigation">
+		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'initializr' ) ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'initializr' ) ); ?></div>
+	</nav><!-- #nav-above -->
+<?php endif; ?>
+
+<?php /* If there are no posts to display, such as an empty archive page */ ?>
+<?php if ( ! have_posts() ) : ?>
+	<div id="post-0" class="post error404 not-found">
+		<h1 class="entry-title"><?php _e( 'Not Found', 'initializr' ); ?></h1>
+		<div class="entry-content">
+			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'initializr' ); ?></p>
+			<?php get_search_form(); ?>
+		</div><!-- .entry-content -->
+	</div><!-- #post-0 -->
+<?php endif; ?>
+
+<?php while ( have_posts() ) : the_post();
+
+$comment_number_template = _n( '1 Comments', '% Comments', get_comments_number(), 'initializr' );
+?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header>
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'initializr' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			</header>
+			<div class="entry-content">
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'initializr' ) ); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'initializr' ), 'after' => '</div>' ) ); ?>
+			</div><!-- .entry-content -->
+
+			<footer>
+			<?php echo timeAgo(); ?> <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'initializr' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">#</a>
+			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'initializr' ), __( '1 Comment', 'initializr' ), __( '% Comments', 'initializr' ) ); ?></span>
+			<?php edit_post_link( __( 'Edit', 'initializr' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+			</footer>	
+		</article>
+
+if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+					<div id="entry-author-info">
+						<div id="author-avatar">
+							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'initializr_author_bio_avatar_size', 60 ) ); ?>
+						</div><!-- #author-avatar -->
+						<div id="author-description">
+							<h2><?php printf( esc_attr__( 'About %s', 'initializr' ), get_the_author() ); ?></h2>
+							<?php the_author_meta( 'description' ); ?>
+							<div id="author-link">
+								<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+									<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'initializr' ), get_the_author() ); ?>
+								</a>
+							</div><!-- #author-link	-->
+						</div><!-- #author-description -->
+					</div><!-- #entry-author-info -->
+<?php endif; ?>
+
+					<div class="entry-utility">
+						<?php initializr_posted_in(); ?>
+						<?php edit_post_link( __( 'Edit', 'initializr' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-utility -->
+
+				<div id="nav-below" class="navigation">
+					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'initializr' ) . '</span> %title' ); ?></div>
+					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'initializr' ) . '</span>' ); ?></div>
+				</div><!-- #nav-below -->
+
+				<?php comments_template( '', true ); ?>
+
+
+<?php endwhile; // End the loop. Whew. ?>
+
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+				<nav id="nav-below" class="navigation">
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'initializr' ) ); ?></div>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'initializr' ) ); ?></div>
+				</nav><!-- #nav-below -->
+<?php endif; ?>
