dce48c7a4b8f329e171947e009f9e8fbcd7eef36
Dougal Campbell <dougal@woody.xen.prgmr.com>
2011-04-23 23:09:09 -0400

    above-header widget position. css ver bump.

--- functions.php
+++ functions.php
@@ -92,7 +92,7 @@ if ( ! function_exists( 'initializr_setup' ) ):
  */
 function initializr_setup() {
         // Load main stylesheet
-	wp_enqueue_style('initializr', get_template_directory_uri() . '/style.css', array(), '0.0.3');
+	wp_enqueue_style('initializr', get_template_directory_uri() . '/style.css', array(), '0.0.4');
 
 	// This theme styles the visual editor with editor-style.css to match the theme style.
 	add_editor_style();
